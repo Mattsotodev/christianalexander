1446e0084417a34b7d641b61f5d3f6b12f3d77e2
Matthew Soto <Matthewsotodev@gmail.com>
2018-10-03 17:03:46 -0400

    initial commit

---
+++ assets/partials/nav.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default affix-top">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false">
+				<span class="glyphicon glyphicon-menu-hamburger"></span>
+			</button>
+		</div>
+		<div class="collapse navbar-collapse" id="navbar-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="index.php">Home</a></li>
+				<li><a href="events.php">Events</a></li>
+                <li><a href="shop.php">Shop</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+		</div>
+	</nav>
---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <title>Home</title>
+    <!--Required metatag and stylesheet-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!--Custom styles here-->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <?php require_once('assets/partials/nav.php'); ?>
+    <div class="container"></div>
+    <!--Required scripts-->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!--Custom script here-->
+    <script>
+        
+    </script>
+
+</body>
+
+</html>
